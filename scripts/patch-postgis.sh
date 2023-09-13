d845c47bb670b9bfe55c42dc2a4e0c48722c7cc9
Christian Mahnke <cmahnke@gmail.com>
2023-09-13 12:48:50 +0200

    Updated patch script

--- scripts/patch-postgis.sh
+++ scripts/patch-postgis.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sed -Ei 's/pg_ctl -D \/tempdb start/pg_ctl -o "--jit=off" -D \/tempdb start/g' docker-postgis/15-3.4/alpine/Dockerfile
-sed -Ei 's/pg_ctl -D \/tempdb start/pg_ctl -o "--jit=off" -D \/tempdb start/g' docker-postgis/14-3.3/alpine/Dockerfile
+sed -Ei 's/pg_ctl -D \/tempdb start/pg_ctl -o "--jit=off" -D \/tempdb start/g' docker-postgis/14-3.4/alpine/Dockerfile
